6263ef177a213543db9cbc4984e7cd47d73d2608
gkfthddk <gkfthddk@gmail.com>
2017-09-06 15:22:02 +0900

    wk working

--- wkrun.py
+++ wkrun.py
@@ -9,6 +9,8 @@ from common import fit,data
 from sklearn.model_selection import train_test_split
 from importlib import import_module
 #from sklearn.metrics import roc_auc_score, auc, precision_recall_curve, roc_curve, average_precision_score
+rand=int(random.random()*10000)
+mx.random.seed(rand)
 parser=argparse.ArgumentParser()
 #parser.add_argument("--batch_size",type=int,default=100,help='the number of batch size')
 #parser.add_argument("--num_epochs",type=int,default=10,help='the number of total epochs')
@@ -23,7 +25,7 @@ data.add_data_args(parser)
 
 
 parser.set_defaults(
-    network = 'wkvgg',
+    network = 'vgg',
     gpus=None,
     num_layers = 18,
     num_classes = 2,
@@ -39,6 +41,7 @@ parser.set_defaults(
 
 
 args=parser.parse_args()
+print rand
 print args
 if(args.begin<0. or args.end>1. or args.begin>=args.end):
     print "must be 0 <= begin < end <= 1"
@@ -54,13 +57,12 @@ start=datetime.datetime.now()
 # test_iter =rootiter('/home/gkfthddk/tutorials/gkfthddk/../jet1.root',['data'],['softmax_label'],batch_size=1000,begin=0.01,end=0.012)
 batch_num=args.batch_size
 print batch_num
-train_iter=wkiter('../jetimgnum.root',['data'],['softmax_label'],batch_size=batch_num,begin=_beg,end=_mid)
+train_iter=wkiter('../jetimgnumtest.root',['data'],['softmax_label'],batch_size=batch_num,begin=_beg,end=_mid)
 test_iter=imiter('../jetimgnum.root',['data'],['softmax_label'],batch_size=batch_num,begin=_mid,end=_end)
 
 net=import_module('symbols.'+args.network)
 sym=net.get_symbol(**vars(args))
 
-print "vgg"
 
 
 import logging
@@ -71,7 +73,6 @@ if("vgg"==args.network):
   print("true")
 model = mx.mod.Module(symbol=sym, context=fit.getctx(args.gpus))
 #model=fit.getmodel(args,sym)
-print "gpu pass"
 # train with the same 
 """
 batch_end_callback = mx.callback.Speedometer(batch_size, 1000),
@@ -84,8 +85,12 @@ optimizer_params={'learning_rate':0.1},
                 #[mx.callback.ProgressBar(train_iter.totalnum()),mx.callback.Speedometer(batch_num,train_iter.totalnum()-1)],
 print train_iter.totalnum(),"batches"
 model.fit(train_iter,
+                eval_data=test_iter,
                 optimizer=args.optimizer,
-                epoch_end_callback=mx.callback.do_checkpoint('save/jetwcheck_'+args.network+'_'+str(start.date())),
+                eval_metric='acc',
+                batch_end_callback =
+                [mx.callback.ProgressBar(train_iter.totalnum()),mx.callback.Speedometer(batch_num,train_iter.totalnum()-1)],
+                epoch_end_callback=mx.callback.do_checkpoint('save/jetwcheck/jetwcheck_'+args.network+'_'+str(start.date())),
                 num_epoch=args.num_epochs)
 #lenet_model.save_checkpoint(prefix='jeti1_1',epoch=10)
 
